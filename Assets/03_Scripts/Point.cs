670fa16991671685eeaa1c392ddf40cd46000bad
SeokhyunDev <kwak1s1h@kakao.com>
2022-05-26 18:10:25 +0900

    asdfa
    a

--- Assets/03_Scripts/Point.cs
+++ Assets/03_Scripts/Point.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Point : MonoBehaviour
+public class Point : MonoBehaviour 
 {
     public static Point Instance;
     private void Awake()
